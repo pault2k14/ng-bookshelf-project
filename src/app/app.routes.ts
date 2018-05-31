33c0b53b10b03825dd9364feb800151a19f55672
Paul Thompson <pbt@pdx.edu>
2018-05-31 00:29:39 -0700

    Added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,15 @@
 import {Routes} from "@angular/router";
+import {BookComponent} from './book/book.component';
+import {PagerComponent} from './pager/pager.component';
+import {HeaderComponent} from './header/header.component';
+import {AppComponent} from './app.component';
+import {BookListComponent} from './book-list/book-list.component';
+import {SearchComponent} from './search/search.component';
+import {LibraryComponent} from './library/library.component';
 
 export const routes: Routes = [
-  //TODO: Define the routes
+  {path: '', redirectTo: 'library', pathMatch: 'full'},
+  {path: 'library', component: LibraryComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'book/:bookid', component: BookComponent}
 ];
